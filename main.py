bdf8fbac17bd43641eaddafbdbe6facd65074b1f
pujan tamang <pujantamang92@gmail.com>
2023-09-18 13:13:32 +0100

    training the dataset and evaluate the model using user input

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth',
                     'PetalLength', 'PetalWidth', 'Species']
 SPECIES = ['Setosa', 'Versicolor', 'Virginica']
+R_FEATURE = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
 
 # get dataset
 train_path = tf.keras.utils.get_file(
@@ -24,7 +25,6 @@ test_y = test.pop('Species')
 my_feature_columns = []
 for key in train.keys():
     my_feature_columns.append(tf.feature_column.numeric_column(key=key))
-print(my_feature_columns)
 '''
 [NumericColumn(key='SepalLength', shape=(1,), default_value=None, dtype=tf.float32, normalizer_fn=None), NumericColumn(key='SepalWidth', shape=(1,), default_value=None, dtype=tf.float32, normalizer_fn=None), NumericColumn(key='PetalLength', shape=(1,), default_value=None, dtype=tf.float32, normalizer_fn=None), NumericColumn(key='PetalWidth', shape=(1,), default_value=None, dtype=tf.float32, normalizer_fn=None)]'''
 
@@ -37,3 +37,65 @@ classifier = tf.estimator.DNNClassifier(
     hidden_units=[30, 10],
     # The model must choose between 3 classes.
     n_classes=3)
+
+
+# convert the data into Dataset format
+def input_fn(features, labels, training=True, batch_size=256):
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    # Shuffle and repeat if you are in training mode.
+    if training:
+        dataset = dataset.shuffle(1000).repeat()
+
+    return dataset.batch(batch_size)
+
+
+# training the model
+classifier.train(
+    input_fn=lambda: input_fn(train, train_y, training=True),
+    steps=5000)
+# We include a lambda to avoid creating an inner function previously
+
+# Evaluate the model accuracy by running the same data
+eval_result = classifier.evaluate(
+    input_fn=lambda: input_fn(test, test_y, training=False))
+
+print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+'''Test set accuracy: 0.900'''
+
+
+def user_input(features, batch_size=256):
+    # Convert the inputs to a Dataset without labels.
+    return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
+
+
+predict = {}
+print("Please type numeric values as prompted.")
+# we ask the user to enter 4 inputs according to the features
+for feature in R_FEATURE:
+    valid = True
+    while valid:
+        val = input(feature + ": ")
+        if not val.isdigit():
+            valid = False
+
+    predict[feature] = [float(val)]
+
+# after the input is complete we put the values to predictions and present the possibility of the species
+predictions = classifier.predict(input_fn=lambda: user_input(predict))
+for pred_dict in predictions:
+    class_id = pred_dict['class_ids'][0]
+    probability = pred_dict['probabilities'][class_id]
+
+    print('Prediction is "{}" ({:.1f}%)'.format(
+        SPECIES[class_id], 100 * probability))
+
+'''
+Please type numeric values as prompted.
+SepalLength: 5.1
+SepalWidth: 5.9
+PetalLength: 1.7
+PetalWidth: 0.5
+...
+Prediction is "Setosa" (97.4%)
+'''
